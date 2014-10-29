84e476d7e7f1134f7de123e6720089a75450e8ba
mirrorecho <info@randallwest.com>
2014-10-28 23:27:18 -0500

    ghost structure

--- ghost_structure.py
+++ ghost_structure.py
@@ -6,21 +6,33 @@ score = Score([])
 
 flute_staff = scoretools.Staff()
 
+# cell = name, pitches
+
 class Ghost:
     #to do... just add any cell... (by name)
     def __init__(self):
-        self.pitches = [1, 0, 5, 8, 7, 1, 3, 6, 5, 0]
-        self.cells = [[0,1], [3,4], [6,7,8]]
-        self.holds = [2, 5, 9]
+        self.cells = {} # each cell is a named list of notes
         self.notes = []
 
-    def add_cell(self, cell_index, durations=[(1,8)]):
-        append_notes = [scoretools.Note(self.pitches[pitch_index], durations[0]) for pitch_index in self.cells[cell_index]]
-        self.notes.extend(append_notes)
+    def slur_cell(self, cell_name):
+        attach(Slur(), self.cells[cell_name])
+
+    def define_cell(self, cell_name, cell_ly):
+        self.cells[cell_name] = [Note(note) for note in cell_ly.split()]
+
+    def add_cell(self, cell_name, transposition=0, durations=[]):
+        #append_notes = [scoretools.Note(self.pitches[pitch_index], durations[0]) for pitch_index in self.cells[cell_index]]
+        #self.notes.extend(append_notes)
+        self.notes.extend(self.cells[cell_name])
 
 ghost1 = Ghost()
-ghost1.add_cell(0, [(1,4)])
-ghost1.add_cell(0)
+ghost1.define_cell('moan', "gf'8 f'4.")
+ghost1._cell('moan', "gf'8 f'4.")
+
+ghost1.slur_cell('moan')
+ghost1.add_cell('moan')
+
+
 
 # to do... output notes as JSON data
 
